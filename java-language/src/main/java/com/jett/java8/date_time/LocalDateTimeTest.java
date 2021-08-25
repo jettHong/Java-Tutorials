3cc944fd4b6c6ef306531653cfbea44967698144
jettHong <hongjunjie@live.com>
2021-08-25 14:47:35 +0800

    时间日期 LocalDateTime\LocalDate\Date

--- java-language/src/main/java/com/jett/java8/date_time/LocalDateTimeTest.java
+++ java-language/src/main/java/com/jett/java8/date_time/LocalDateTimeTest.java
@@ -18,8 +18,12 @@ import static java.time.temporal.TemporalAdjusters.*;
  */
 public class LocalDateTimeTest {
     
-    
-    public static void main(String[] args) {
+    /**
+     * 一、创建
+     */
+    @Test
+    public void create() {
+        
         // =================== 年月日 LocalDate =================
         {
             //获取当前年月日
@@ -90,7 +94,12 @@ public class LocalDateTimeTest {
             // 获取毫秒数（推荐）
             System.currentTimeMillis();
         }
-        
+    }
+    
+    /**
+     * 时间计算
+     */
+    public void calc() {
         // =================== 时间计算-增减 =================
         {
             // 注意：LocalDate、LocalTime、LocalDateTime、Instant为不可变对象，修改这些对象对象会返回一个副本
@@ -121,8 +130,14 @@ public class LocalDateTimeTest {
             // 取得当前月份最后一天
             System.out.println(nowDate.with(lastDayOfMonth()));
         }
-        
-        // =================== 格式化时间（转字串） =================
+    }
+    
+    /**
+     * 与 String 互转
+     */
+    @Test
+    public void convertWithString() {
+        // =================== 格式化时间（日期转字串） =================
         {
             LocalDate localDate = LocalDate.of(2019, 9, 10);
             // 输成字串
@@ -146,36 +161,89 @@ public class LocalDateTimeTest {
             assert localDate1 == localDate2;
             System.out.println(localDate1);
         }
-        
-        // =================== 与原有Date互相转换 =================
+    }
+    
+    /**
+     * 与 Date 互相转换
+     */
+    @Test
+    public void convertWithDate() {
+        // ===== 从 Date 转成 Local =====
         {
-            // 从 Date 转成 Local
             Date date = new Date();
+            // 直接 date.toInstant() 转换成 Instant
             Instant instant = date.toInstant();
             // 取得系统默认区划ID
             ZoneId zoneId = ZoneId.systemDefault();
             // 取得东八区
             ZoneId gmt8 = ZoneId.of("+8");
+            
+    
             // 方法1 ofInstant
             LocalDateTime localDateTime1 = LocalDateTime.ofInstant(instant, zoneId);
-            // 方法1 ZonedDateTime.toLocalDateTime
+            // 方法2 ZonedDateTime.toLocalDateTime
             LocalDateTime localDateTime2 = instant.atZone(zoneId).toLocalDateTime();
-            
-            // 从 Local 转成 Date
-            Date dateBack = Date.from(localDateTime1.atZone(zoneId).toInstant());
-            
+            // 方法3 通过 Instant.ofEpochMilli(date.getTime())
+            // 抽取成工具类，使用方法3
+            System.out.println(asLocalDate(date));
+            System.out.println(asLocalDateTime(date));
+        }
+        
+        // ===== 从 Local 转成 Date =====
+        {
+            LocalDateTime localDateTime = LocalDateTime.now();
+            Date dateBack = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    
+            System.out.println(asDate(LocalDate.now()));
+            System.out.println(asDate(LocalDateTime.now()));
         }
     }
     
     /**
-     * 日期差
-     * REF：https://blog.csdn.net/u012240455/article/details/89670013
+     * Date => LocalDate
+     * @param date
+     * @return
      */
+    public static LocalDate asLocalDate(Date date) {
+        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+    
+    /**
+     * Date => LocalDateTime
+     * @param date
+     * @return
+     */
+    public static LocalDateTime asLocalDateTime(Date date) {
+        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+    
+    /**
+     * 转成日期Date
+     * @param localDate
+     * @return
+     */
+    public static Date asDate(LocalDate localDate) {
+        return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+    }
+    
+    /**
+     * 转成日期Date
+     * @param localDateTime
+     * @return
+     */
+    public static Date asDate(LocalDateTime localDateTime) {
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+        
+        /**
+         * 日期时间差
+         * REF：https://blog.csdn.net/u012240455/article/details/89670013
+         */
     @Test
     public void dateDiffTest() {
         LocalDate from = LocalDate.of(2018, 5, 12);
         LocalDate to = LocalDate.of(2021, 6, 17);
-    
+        
         System.out.println(" ========== ChronoUnit，推荐 ========== ");
         System.out.println(ChronoUnit.DAYS.between(from, to));
         System.out.println(ChronoUnit.MONTHS.between(from, to));
@@ -184,13 +252,13 @@ public class LocalDateTimeTest {
         // 1132
         // 37
         // 3
-    
+        
         System.out.println(" ========== Period 不好用!!! ========== ");
         Period between = Period.between(from, to);
         System.out.println("天：" + between.getDays());
         System.out.println("月：" + between.getMonths());
         System.out.println("年：" + between.getYears());
-        // 只有输出,单个
+        // 只有输出单个字段比对值！！！
         // 天：5
         // 月：1
         // 年：3
@@ -207,8 +275,6 @@ public class LocalDateTimeTest {
         System.out.println("相差秒数（不包含纳秒）：" + duration.getSeconds());
         System.out.println("相差纳秒数（不包含秒）：" + duration.getNano());
         System.out.println(duration.getUnits());
-        
-        
     }
     
 }
